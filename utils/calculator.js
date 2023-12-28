aeac0bfa79e7ed47248be6dee4596956fdade731
vjkarthik1987 <vjkarthik.2987@gmail.com>
2023-12-28 10:27:01 +0530

    Add formula for 0 length array

--- utils/calculator.js
+++ utils/calculator.js
@@ -1,6 +1,9 @@
 const Expense = require('../models/Expense');
 
 const sumField = function convertArray(arr, field) {
+    if (arr.length == 0) {
+        return 0;
+    }
     const convertedArr =  arr.map(arrItem => arrItem[field]);
     return convertedArr.reduce(function (acc, curr) {
         return acc + curr;
